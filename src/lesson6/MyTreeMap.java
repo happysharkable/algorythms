57f5cb8ab57130d2c300113278e0bf780eeca2eb
Alexander Bondarenko <happy-shark@ya.ru>
2020-08-08 12:39:55 +0300

    Homework06

--- src/lesson6/MyTreeMap.java
+++ src/lesson6/MyTreeMap.java
@@ -19,6 +19,22 @@ public class MyTreeMap<Key extends Comparable<Key>, Value> {
         }
     }
 
+    public int height() { return height(root); }
+
+    private int height(Node node) {
+        if (node == null || node.left == null && node.right == null) {
+            return 0;
+        }
+        return Math.max(height(node.left) + 1, height(node.right) + 1);
+    }
+
+    public boolean isBalanced() { return isBalanced(root); }
+
+    private boolean isBalanced(Node node) {
+        if (height(node) == 0) return true;
+        return (Math.abs(height(node.left) - height(node.right)) <= 1);
+    }
+
     public int size() {
         return size(root);
     }
